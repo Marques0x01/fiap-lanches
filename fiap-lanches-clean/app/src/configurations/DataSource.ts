82505f4ac2d705b1468e5933f272252da9d34c34
jonathansantoss <jonathan.gomess1@gmail.com>
2024-03-16 17:23:46 -0300

    Update DataSource.ts

--- fiap-lanches-clean/app/src/configurations/DataSource.ts
+++ fiap-lanches-clean/app/src/configurations/DataSource.ts
@@ -24,7 +24,7 @@ require('dotenv').config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: 80,
+  port: 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
