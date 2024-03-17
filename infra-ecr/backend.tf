f8c36fbaef4c0a0859433760d16bcecbad35b995
Fabio Marques <fabio.marquezzx@gmail.com>
2024-03-17 10:15:21 -0300

    feat: changing to sa-east-1

--- infra-ecr/backend.tf
+++ infra-ecr/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "fiap-tfstates"
     key            = "fiap-lanches-app/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "sa-east-1"
     
   }
 }
